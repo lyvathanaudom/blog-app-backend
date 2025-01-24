246e6a5756ae0628b22e9dc81e9446e405ba473a
Vathana Park <90886658+lyvathanaudom@users.noreply.github.com>
2025-01-24 21:43:19 +0700

    add more domain to cors

--- src/index.js
+++ src/index.js
@@ -1,44 +1,47 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const cors = require('cors');
-const basicAuth = require('express-basic-auth');
-const postsRoute = require('./routes/posts');
+const express = require("express");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const cors = require("cors");
+const basicAuth = require("express-basic-auth");
+const postsRoute = require("./routes/posts");
 dotenv.config();
 
 const app = express();
 // CORS configuration
 const corsOptions = {
-    origin: 'http://localhost:3000', // Replace with your Vue frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-    optionsSuccessStatus: 200
-  };
-  
-  // Apply CORS middleware
-  app.use(cors(corsOptions));
-  
+  origin: ["http://localhost:3000", "https://leearchive.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.use(helmet())
+app.use(helmet());
 app.use((req, res, next) => {
-    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-    next();
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
 });
-app.use(basicAuth({
+app.use(
+  basicAuth({
     users: { [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PASSWORD },
     challenge: true,
-    unauthorizedResponse: 'Unauthorized access. Please provide valid credentials.'
-}));
+    unauthorizedResponse:
+      "Unauthorized access. Please provide valid credentials.",
+  })
+);
 app.get("/", (req, res) => {
-    res.send("success");
+  res.send("success");
 });
 
 // Use the posts route
-app.use('/api/posts', postsRoute); 
+app.use("/api/posts", postsRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-    console.log('Environment:', process.env.NODE_ENV || 'development');
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log("Environment:", process.env.NODE_ENV || "development");
 });
